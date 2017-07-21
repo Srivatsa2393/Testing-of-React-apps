320a3067a6190467982f2b5c7039506d1de54048
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-21 21:55:15 +0530

    testing class names

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
-class CommentBox extends Component{
+export default class CommentBox extends Component{
   render() {
     return(
-
+      <div className="comment-box">
+        <textarea />
+        <button>
+          Submit Comment
+        </button>
+      </div>
     );
   }
 }
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,6 +3,11 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
 
+  it('has the correct class', () => {
+    const component = renderComponent(CommentBox);
+    expect(component).to.class('comment-box');
+  });
+
   it('has a textarea', () => {
     //what matchers are needed chai-jquery
     const component = renderComponent(CommentBox);
@@ -10,6 +15,7 @@ describe('CommentBox', () => {
   });
 
   it('has a button', () => {
-
+    const component = renderComponent(CommentBox);
+    expect(component.find('button')).to.exist;
   });
 });
