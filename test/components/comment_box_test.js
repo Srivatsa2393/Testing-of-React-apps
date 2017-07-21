d84c2f6ae6fe6cea3d67cc65a335a514f8d3ed9c
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-21 21:42:52 +0530

    comment box test

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -4,7 +4,9 @@ import CommentBox from '../../src/components/comment_box';
 describe('CommentBox', () => {
 
   it('has a textarea', () => {
-
+    //what matchers are needed chai-jquery
+    const component = renderComponent(CommentBox);
+    expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
