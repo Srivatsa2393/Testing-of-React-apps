42a5d419139f59688f474d3fb7b1b66ca59f2725
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-21 21:31:49 +0530

    Test structure

---
+++ test/components/comment_box_test.js
@@ -0,0 +1,13 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox', () => {
+
+  it('has a textarea', () => {
+
+  });
+
+  it('has a button', () => {
+
+  });
+});
