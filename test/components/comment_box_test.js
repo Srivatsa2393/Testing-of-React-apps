42d108d876575f028182729b8bc84b43138aa2bb
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-23 03:21:22 +0530

    using the beforeEach() to condense tests

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,20 +2,23 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  let component;
+
+  //beforeEach runs first and gets called three times
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
 
   it('has the correct class', () => {
-    const component = renderComponent(CommentBox);
     expect(component).to.class('comment-box');
   });
 
   it('has a textarea', () => {
     //what matchers are needed chai-jquery
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
 });
