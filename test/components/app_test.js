41bf23724a6709d0216f56f07dd26d781185f77c
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-23 03:32:25 +0530

    Expecting child elements

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,35 +1,32 @@
-// import { renderComponent , expect } from '../test_helper';
-// import App from '../../src/components/app';
-//
-// describe('App' , () => {
-//   let component;
-//
-//   beforeEach(() => {
-//     component = renderComponent(App);
-//   });
-//
-//   it('renders something', () => {
-//     expect(component).to.exist;
-//   });
-// });
-
 //rerunning of test again and again
 import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
+// // Use "describe" to group together similar tests
+// describe('App', () => {
+//
+//   // Use "it" to test a single attribute of a target
+//   it('shows the correct text', () => {
+//
+//     //create an instance of App
+//     const component = renderComponent(App);
+//
+//     // Use "expect" to make an 'assertion' about a target
+//     expect(component).to.contain('React simple starter');
+//
+//   });
+
 // Use "describe" to group together similar tests
 describe('App', () => {
+let component;
 
-  // Use "it" to test a single attribute of a target
-  it('shows the correct text', () => {
+beforeEach(() => {
+  component = renderComponent(App);
+})
 
-    //create an instance of App
-    const component = renderComponent(App);
-
-    // Use "expect" to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter');
-
-  });
+it('shows a comment box', () => {
+  expect(component.find('.comment-box')).to.exist;
+});
 
 
 });
