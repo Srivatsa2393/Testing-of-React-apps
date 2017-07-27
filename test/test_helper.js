515e91a23a1ab0041258dbba05964fc3c411327a
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-27 15:14:25 +0530

    More jsdom setup, setting up a fake html document

--- test/test_helper.js
+++ test/test_helper.js
@@ -39,10 +39,12 @@
 
 
 import jsdom from 'jsdom';
+import jquery from 'jquery';
 
 // Setup a testing environment to run like a browser in the command line
   global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
   global.window = global.document.defaultView;
+  const $ = jquery(global.window);
 
 //We need to build 'renderComponent' helper that should render a givem react ComponentClass
 
