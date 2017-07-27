0f08549a9d0a4f657756af832c3d42c455509f60
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-27 15:40:53 +0530

    Defining render component

--- test/test_helper.js
+++ test/test_helper.js
@@ -42,6 +42,11 @@ import jsdom from 'jsdom';
 import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 
 
 // Setup a testing environment to run like a browser in the command line
@@ -51,7 +56,11 @@ import ReactDOM from 'react-dom';
 
 //We need to build 'renderComponent' helper that should render a given react ComponentClass
   function renderComponent(ComponentClass) {
-    const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+    const componentInstance = TestUtils.renderIntoDocument(
+      <Provider store={createStore(reducers)}>
+        <ComponentClass />
+      </Provider>
+    );
 
     return $(ReactDOM.findDOMNode(componentInstance)); //produces the HTML
   }
@@ -61,3 +70,4 @@ import ReactDOM from 'react-dom';
 
 
 //Set up chai-jquery
+  export { renderComponent, expect };
